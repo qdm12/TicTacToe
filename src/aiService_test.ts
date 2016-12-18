56f4a29f79677aedcbc5ee0296bf75880dcf28ce
qdm12 <qdm12@imperial.ac.uk>
2016-12-17 21:53:56 -0500

    Updated aiService to work with new AI

--- src/aiService_test.ts
+++ src/aiService_test.ts
@@ -1,6 +1,7 @@
 describe("aiService", function() {
-  let W_TURN = 0;
-  let B_TURN = 1;
+    let W_TURN = 0;
+    let B_TURN = 1;
+    let random_start_value = 0.2; //for pawn
   
     function createState(board: Board,
                          isUnderCheck:[boolean,boolean],
@@ -37,19 +38,15 @@ describe("aiService", function() {
     let numberOfTimesCalledRandom = 0;
     Math.random = function () {
         numberOfTimesCalledRandom++;
-        switch(numberOfTimesCalledRandom){
-            case 1: return 0.2134;
-            case 2: return 0.1752;
-            case 3: return 0.6658;
-            case 4: return 0.8875;
-            case 5: return 0.4821;
-            case 6: return 0.8952;
-            case 7: return 0.9877;
-            default: throw new Error("Called Math.random more times than expected");
+        if(numberOfTimesCalledRandom >= 100){
+            //we are stuck, we need to change value (no more possible moves)
+            return 0.65; //for knight
         }
+        return random_start_value;
     };
   
-  it("createComputerMove returns random-ring move for Pawn' index from White", function() {
+  it("createComputerMove returns deterministic random move for Pawn from White", function() {
+    random_start_value = 0.2; //for pawn
     let next_move:IMove = createComputerMove(
         W_TURN,
         null, //endMatchScores
@@ -68,10 +65,163 @@ describe("aiService", function() {
         [true,true], //canCastleQueen
         {row:null, col:null}); //enpassantPosition
     expect(next_move.stateAfterMove.delta.deltaFrom.row).toBe(6);
-    expect(next_move.stateAfterMove.delta.deltaFrom.col).toBe(0);
+    expect(next_move.stateAfterMove.delta.deltaFrom.col).toBe(4);
     expect(next_move.stateAfterMove.delta.deltaTo.row).toBe(5);
+    expect(next_move.stateAfterMove.delta.deltaTo.col).toBe(4);
+  });
+  
+  it("createComputerMove returns deterministic random move for Rook from White", function() {
+    random_start_value = 0.45; //for rook
+    let next_move:IMove = createComputerMove(
+        W_TURN,
+        null, //endMatchScores
+        [
+        ['BR', 'BN', 'BB', 'BQ', 'BK', 'BB', 'BN', 'BR'],
+        ['BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP'],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['WP',   '',   '',   '',   '',   '',   '',   ''],
+        ['WR', '', '', '', 'WK', '', '', '']
+        ],
+        [false,false], //isUnderCheck
+        [true,true], //canCastleKing
+        [true,true], //canCastleQueen
+        {row:null, col:null}); //enpassantPosition
+    expect(next_move.stateAfterMove.delta.deltaFrom.row).toBe(7);
+    expect(next_move.stateAfterMove.delta.deltaFrom.col).toBe(0);
+    expect(next_move.stateAfterMove.delta.deltaTo.row).toBe(7);
+    expect(next_move.stateAfterMove.delta.deltaTo.col).toBe(1);
+  });
+  
+  it("createComputerMove returns deterministic random move for Knight from White", function() {
+    random_start_value = 0.51; //for knight
+    let next_move:IMove = createComputerMove(
+        W_TURN,
+        null, //endMatchScores
+        [
+        ['BR', 'BN', 'BB', 'BQ', 'BK', 'BB', 'BN', 'BR'],
+        ['BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP'],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['WN', '', '', '', 'WK', '', '', '']
+        ],
+        [false,false], //isUnderCheck
+        [true,true], //canCastleKing
+        [true,true], //canCastleQueen
+        {row:null, col:null}); //enpassantPosition
+    expect(next_move.stateAfterMove.delta.deltaFrom.row).toBe(7);
+    expect(next_move.stateAfterMove.delta.deltaFrom.col).toBe(0);
+    expect(next_move.stateAfterMove.delta.deltaTo.row).toBe(6);
+    expect(next_move.stateAfterMove.delta.deltaTo.col).toBe(2);
+  });
+  
+  it("createComputerMove returns deterministic random move for Bishop from White", function() {
+    random_start_value = 0.75; //for bishop
+    let next_move:IMove = createComputerMove(
+        W_TURN,
+        null, //endMatchScores
+        [
+        ['BR', 'BN', 'BB', 'BQ', 'BK', 'BB', 'BN', 'BR'],
+        ['BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP'],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['',   '',   'WP',   '',   '',   '',   '',   ''],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['WB', '', '', '', 'WK', '', '', '']
+        ],
+        [false,false], //isUnderCheck
+        [true,true], //canCastleKing
+        [true,true], //canCastleQueen
+        {row:null, col:null}); //enpassantPosition
+    expect(next_move.stateAfterMove.delta.deltaFrom.row).toBe(7);
+    expect(next_move.stateAfterMove.delta.deltaFrom.col).toBe(0);
+    expect(next_move.stateAfterMove.delta.deltaTo.row).toBe(6);
+    expect(next_move.stateAfterMove.delta.deltaTo.col).toBe(1);
+  });
+  
+  it("createComputerMove returns deterministic random move for Queen from White", function() {
+    random_start_value = 0.87; //for queen
+    let next_move:IMove = createComputerMove(
+        W_TURN,
+        null, //endMatchScores
+        [
+        ['BR', 'BN', 'BB', 'BQ', 'BK', 'BB', 'BN', 'BR'],
+        ['BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP'],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['',   '',   'WP',   '',   '',   '',   '',   ''],
+        ['WP',   '',   '',   '',   '',   '',   '',   ''],
+        ['WQ', 'WP', '', '', 'WK', '', '', '']
+        ],
+        [false,false], //isUnderCheck
+        [true,true], //canCastleKing
+        [true,true], //canCastleQueen
+        {row:null, col:null}); //enpassantPosition
+    expect(next_move.stateAfterMove.delta.deltaFrom.row).toBe(7);
+    expect(next_move.stateAfterMove.delta.deltaFrom.col).toBe(0);
+    expect(next_move.stateAfterMove.delta.deltaTo.row).toBe(6);
+    expect(next_move.stateAfterMove.delta.deltaTo.col).toBe(1);
+  });
+  
+  
+  it("createComputerMove returns deterministic random move for King from White", function() {
+    random_start_value = 0.96; //for king
+    let next_move:IMove = createComputerMove(
+        W_TURN,
+        null, //endMatchScores
+        [
+        ['BR', 'BN', 'BB', 'BQ', 'BK', 'BB', 'BN', 'BR'],
+        ['BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP'],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['WK', '', '', '', '', '', '', '']
+        ],
+        [false,false], //isUnderCheck
+        [true,true], //canCastleKing
+        [true,true], //canCastleQueen
+        {row:null, col:null}); //enpassantPosition
+    expect(next_move.stateAfterMove.delta.deltaFrom.row).toBe(7);
+    expect(next_move.stateAfterMove.delta.deltaFrom.col).toBe(0);
+    expect(next_move.stateAfterMove.delta.deltaTo.row).toBe(6);
     expect(next_move.stateAfterMove.delta.deltaTo.col).toBe(0);
   });
+  
+  it("createComputerMove returns deterministic random move for knight after trying pawn from White", function() {
+    random_start_value = 0.2; //Start with pawn
+    //then it will reach 100 and go to the knight choice
+    let next_move:IMove = createComputerMove(
+        W_TURN,
+        null, //endMatchScores
+        [
+        ['BR', 'BN', 'BB', 'BQ', 'BK', 'BB', 'BN', 'BR'],
+        ['', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP'],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['',   '',   '',   '',   '',   '',   '',   ''],
+        ['BP',   '',   '',   '',   '',   '',   '',   ''],
+        ['WP',   '',   '',   '',   '',   '',   '',   ''],
+        ['WN', '', '', '', '', '', '', 'WK']
+        ],
+        [false,false], //isUnderCheck
+        [true,true], //canCastleKing
+        [true,true], //canCastleQueen
+        {row:null, col:null}); //enpassantPosition
+    expect(next_move.stateAfterMove.delta.deltaFrom.row).toBe(7);
+    expect(next_move.stateAfterMove.delta.deltaFrom.col).toBe(0);
+    expect(next_move.stateAfterMove.delta.deltaTo.row).toBe(6);
+    expect(next_move.stateAfterMove.delta.deltaTo.col).toBe(2);
+  });
+  
   
   it("createComputerMove returns attack move from White' index", function() {
     let next_move:IMove = createComputerMove(
